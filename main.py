31409f61217b7be40e1f4f838e376a917481a3d4
CoconutMacaroon <arjunvedbrat@outlook.com>
2020-03-13 09:58:31 -0700

    add code

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import Xlib.display
-display = Xlib.display.Display()
-window = display.get_input_focus().focus
-wmname = window.get_wm_name()
-wmclass = window.get_wm_class()
-if wmclass is None and wmname is None:
-    window = window.query_tree().parent
-    wmname = window.get_wm_name()
-print "WM Name: %s" % ( wmname, )
+from win32gui import GetWindowText, GetForegroundWindow
+from time import sleep
+print(GetWindowText(GetForegroundWindow()))
+while True:
+    focused_window = GetWindowText(GetForegroundWindow)
+    
