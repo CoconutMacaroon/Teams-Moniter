4d6550045799f80f5d0bcb407db267457fdda794
CoconutMacaroon <arjunvedbrat@outlook.com>
2020-03-13 09:03:34 -0700

    Add code

--- SerialLEDMoniter/SerialLEDMoniter.ino
+++ SerialLEDMoniter/SerialLEDMoniter.ino
@@ -1,1 +1,14 @@
-"\n" 
+void setup() {
+  Serial.begin(9600);     // Communication started with 9600 baud
+  pinMode(2, OUTPUT);
+}
+void loop() {
+  char incomingByte = Serial.read(); // read the incoming byte:
+  if (incomingByte == '1') {
+    digitalWrite(2, HIGH);
+  }
+  if (incomingByte == '0') {
+    digitalWrite(2, LOW);
+  }
+  delay(1000);
+}
